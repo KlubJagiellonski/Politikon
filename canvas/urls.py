988bd768309305ac2967eb5f2149c7f4f465ef88
Tomek Kopczuk <tomek@bladepolska.com>
2013-03-10 14:46:34 +0100

    FB real time updates ping support.

--- canvas/urls.py
+++ canvas/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import patterns, url
 
+from .views import RealtimeUpdates
+
 urlpatterns = patterns('',
     url(r'^$', 'canvas.views.home', name="home"),
+    url(r'^api/v1/realtime/$', RealtimeUpdates.as_view(), name="realtime_update"),
 )
--- canvas/views.py
+++ canvas/views.py
@@ -1,5 +1,9 @@
 from coffin.shortcuts import render_to_response
+
+from django.conf import settings
+from django.http import HttpResponse, HttpResponseForbidden
 from django.template import RequestContext
+from django.views.generic.base import View
 
 from events.models import *
 
@@ -14,3 +18,14 @@ def home(request):
     }
 
     return render_to_response('canvas/home.html', ctx, RequestContext(request))
+
+
+class RealtimeUpdates(View):
+    def get(self, request):
+        if settings.FACEBOOK_REALTIME_VERIFY_TOKEN != request.GET.get('hub.verify_token'):
+            return HttpResponseForbidden("")
+
+        return HttpResponse(request.GET.get("hub.challenge", ""))
+
+    def post(self, request):
+        pass
