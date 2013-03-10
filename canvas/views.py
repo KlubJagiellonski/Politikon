a26819746508e72ff58fbe56364f7a51f343a867
Tomek Kopczuk <tomek@bladepolska.com>
2013-03-10 14:54:50 +0100

    FB RT updates signature checking.

--- canvas/views.py
+++ canvas/views.py
@@ -9,6 +9,9 @@ from events.models import *
 
 from fandjango.decorators import facebook_authorization_required
 
+import hashlib
+import hmac
+
 
 @facebook_authorization_required
 def home(request):
@@ -21,6 +24,17 @@ def home(request):
 
 
 class RealtimeUpdates(View):
+    def check_signature(self, request):
+        body = request.body
+        challenge = u'sha1=' + hmac.new(
+            settings.FACEBOOK_APPLICATION_SECRET_KEY,
+            msg=body,
+            digestmod=hashlib.sha1
+        ).hexdigest()
+
+        if request.META[u'X_HUB_SIGNATURE'] == challenge:
+            return True
+
     def get(self, request):
         if settings.FACEBOOK_REALTIME_VERIFY_TOKEN != request.GET.get('hub.verify_token'):
             return HttpResponseForbidden("")
@@ -28,4 +42,7 @@ class RealtimeUpdates(View):
         return HttpResponse(request.GET.get("hub.challenge", ""))
 
     def post(self, request):
+        if not self.check_signature(request):
+            return HttpResponseForbidden("Bad signature.")
+
         pass
