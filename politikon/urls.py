13d91f69b991d90cac14fda25b08a3b6851aaf7d
Jakub Lipinski <Jakub.Lipinski@gmail.com>
2014-11-26 22:58:18 +0100

    fixing imports

--- politikon/urls.py
+++ politikon/urls.py
@@ -2,6 +2,8 @@ from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
+from events import views
+
 from django.contrib import admin
 admin.autodiscover()
 
