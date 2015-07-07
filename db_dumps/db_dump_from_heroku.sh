4e2963639aedade0b365318b7647e00f18fb042b
Tomasz Grynfelder <tomasz.grynfelder@polidea.com>
2015-07-07 18:54:48 +0200

    db_dump_from_heroku fixed to run properly on docker container's Ubuntu;

--- db_dumps/db_dump_from_heroku.sh
+++ db_dumps/db_dump_from_heroku.sh
@@ -4,7 +4,7 @@
 
 DUMP=`heroku pg:backups --app politikon | grep '^[a|b]' | head -1 | cut -f1 -d' '`
 FILENAME=`heroku pg:backups --app politikon | grep '^[a|b]' | head -1 | cut -f3,4 -d' '`
-FILENAME=`echo "politikon_NON-anonymized_db_${FILENAME// /_}.dump"`
+FILENAME=`echo $FILENAME | sed 's/:/_/g' | sed 's/ /_/g'`
+FILENAME=`echo "politikon_NON-anonymized_db_${FILENAME}.dump"`
 
 curl -o $FILENAME `heroku pg:backups public-url $DUMP --app politikon`
-
