9b68009681b2cc424b1d3fdd60797426dea9584e
Piotr Pęczek <ppeczek@gmail.com>
2015-07-14 20:17:17 +0200

    Templatetags instead of macros.

---
+++ events/templatetags/display.py
@@ -0,0 +1,42 @@
+from django import template
+
+
+register = template.Library()
+
+@register.inclusion_tag('render_bet.html')
+def render_bet(event, bet, render_current):
+    return {
+        'event': event,
+        'bet': bet,
+        'render_current': render_current
+    }
+
+@register.inclusion_tag('render_event.html')
+def render_event(event, bet, people):
+    return {
+        'event': event,
+        'bet': bet,
+        'people': people
+    }
+
+@register.inclusion_tag('render_events.html')
+def render_events(events, bets, people):
+    return {
+        'events': events,
+        'bets': bets,
+        'people': people
+    }
+
+@register.inclusion_tag('render_featured_event.html')
+def render_featured_event(event, people):
+    return {
+        'event': event,
+        'people': people
+    }
+
+@register.inclusion_tag('render_featured_events.html')
+def render_featured_events(events, people):
+    return {
+        'events': events,
+        'people': people
+    }
