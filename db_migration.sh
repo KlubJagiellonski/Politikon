1f2a2cafb2416ab5b212a41ef213b82c520ad171
Tomasz Grynfelder <tomasz.grynfelder@polidea.com>
2015-07-14 20:09:10 +0200

    added comment to db_migration.sh;

--- db_migration.sh
+++ db_migration.sh
@@ -1,2 +1,10 @@
 #!/usr/bin/env bash
+
+#python manage.py schemamigration accounts --initial
+#python manage.py schemamigration events --initial
+#
+#python manage.py migrate accounts
+#python manage.py migrate events
+
+# TODO: to be checked if it's sufficient
 python manage.py migrate
