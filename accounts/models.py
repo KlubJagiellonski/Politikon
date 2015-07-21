d5d5988c19f48e68c617866f45735716ee841430
Piotr Pęczek <ppeczek@gmail.com>
2015-07-22 00:44:31 +0200

    Isolated utils from models and views.

--- accounts/models.py
+++ accounts/models.py
@@ -12,34 +12,10 @@ from bladepolska.snapshots import SnapshotAddon
 from constance import config
 
 from .managers import UserProfileManager
+from .utils import format_int, save_profile
 
 
 logger = logging.getLogger(__name__)
-
-
-def format_int(x):
-    s = str(int(x))
-    l = int((len(s) - 1) / 3)
-    for i in range(l, 0, -1):
-        s = s[:len(s) - i * 3] + ' ' + s[len(s) - i * 3:]
-    return s
-
-
-def save_profile(backend, user, response, *args, **kwargs):
-    print user
-    print response
-    print backend.name
-
-    if backend.name == 'facebook':
-        user.full_name = response['name']
-        user.facebook_id = response['id']
-        user.save(using=UserProfileManager.db)
-        print backend
-    if backend.name == 'twitter':
-        user.avatarURL = response['profile_image_url']
-        user.full_name = response['name']
-        user.save(using=UserProfileManager.db)
-        print backend
 
 
 class UserProfile(AbstractBaseUser):
--- events/views.py
+++ events/views.py
@@ -1,3 +1,7 @@
+import json
+import logging
+logger = logging.getLogger(__name__)
+
 from django.shortcuts import render_to_response
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
@@ -10,58 +14,12 @@ from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
 from django.views.generic import DetailView, ListView
 
-from bladepolska.http import JSONResponse, JSONResponseBadRequest
-import json
-import logging
-logger = logging.getLogger(__name__)
-
 from .exceptions import NonexistantEvent, PriceMismatch, EventNotInProgress, \
     UnknownOutcome, InsufficientBets, InsufficientCash
 from .models import Event, Bet, Transaction
+from .utils import create_bets_dict
 
-from django.contrib.auth.decorators import login_required
-
-
-def create_bets_dict(user, events):
-    bets = dict()
-    if user is not None:
-        bets = Bet.objects.get_users_bets_for_events(user, events)
-        bets = dict((bet.event_id, bet) for bet in bets)
-
-    all_bets = dict()
-    if len(events) > 1:
-        for event in events:
-            if event.id in bets and bets[event.id].has>0:
-                bet = bets[event.id]
-                all_bets[event.id]={
-                    'has_any' : True,
-                    'buyYES': bet.outcome,
-                    'buyNO' : not bet.outcome,
-                    'outcomeYES' : "YES" if bet.outcome else "NO",
-                    'outcomeNO' : "YES" if bet.outcome else "NO",
-                    'priceYES' : event.current_buy_for_price if bet.outcome else event.current_sell_against_price,
-                    'priceNO' : event.current_sell_for_price if bet.outcome else event.current_buy_against_price,
-                    'textYES' : "+" if bet.outcome else "-",
-                    'textNO' : "-" if bet.outcome else "+",
-                    'has' : bet.has,
-                    'classOutcome' : "YES" if bet.outcome else "NO",
-                    'textOutcome' : "TAK" if bet.outcome else "NIE",
-                    'avgPrice' : round(bet.bought_avg_price,2),
-                }
-            else:
-                all_bets[event.id]={
-                    'has_any' : False,
-                    'buyYES': True,
-                    'buyNO' : True,
-                    'outcomeYES' : "YES",
-                    'outcomeNO' : "NO",
-                    'priceYES' : event.current_buy_for_price,
-                    'priceNO' : event.current_buy_against_price,
-                    'textYES' : "TAK",
-                    'textNO' : "NIE"
-                }
-
-    return all_bets
+from bladepolska.http import JSONResponse, JSONResponseBadRequest
 
 
 class EventsListView(ListView):
