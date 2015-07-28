b4e1704f27dc9afa1521e84414676bcccc3ac598
Piotr Pęczek <ppeczek@gmail.com>
2015-07-28 21:04:54 +0200

    no basicauth no sslify

--- politikon/settings.py
+++ politikon/settings.py
@@ -181,9 +181,9 @@ MIDDLEWARE_CLASSES = (
     # forcing one hostname on production
     'politikon.modules.HostnameRedirectMiddleware',
     # forcing SSL using https://github.com/rdegges/django-sslify. This need to be the first middleware
-    'sslify.middleware.SSLifyMiddleware',
+    # 'sslify.middleware.SSLifyMiddleware',
     # adding basic auth
-    'politikon.modules.BasicAuthMiddleware',
+    # 'politikon.modules.BasicAuthMiddleware',
     # 'bladepolska.middleware.InstrumentMiddleware',
     # 'canvas.middleware.FacebookMiddleware',
     #'django.middleware.transaction.TransactionMiddleware',
