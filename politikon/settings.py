510c798777deabec2a9d4f5c552cad919559eba9
Piotr Pęczek <ppeczek@gmail.com>
2015-07-28 21:13:00 +0200

    + sslify

--- politikon/settings.py
+++ politikon/settings.py
@@ -181,7 +181,7 @@ MIDDLEWARE_CLASSES = (
     # forcing one hostname on production
     'politikon.modules.HostnameRedirectMiddleware',
     # forcing SSL using https://github.com/rdegges/django-sslify. This need to be the first middleware
-    # 'sslify.middleware.SSLifyMiddleware',
+    'sslify.middleware.SSLifyMiddleware',
     # adding basic auth
     'politikon.modules.BasicAuthMiddleware',
     # 'bladepolska.middleware.InstrumentMiddleware',
