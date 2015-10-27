f020ee05bd93977ee07e7faac1a7ca0d0a9d2a99
Wojciech Zając <wojciech.zajac@tirasu.com>
2015-10-27 22:08:07 +0100

    Profile list and profile detail user - base methods

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, url
 
-from .views import LogoutView
+from .views import LogoutView, UsersView, UserDetailView
 
 
 urlpatterns = patterns('',
     url(r'^login/$', 'accounts.views.login', name='login'),
     url(r'^logout/$', LogoutView.as_view(), name='logout'),
+    url(r'^users/$', UsersView.as_view(), name='users'),
+    url(r'^(?P<pk>[0-9]+)/$', UserDetailView.as_view(), name='user'),
     # url(r'^logout/$', 'accounts.views.logout', name="accounts"),
 )
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponsePermanentRedirect
 from django.contrib.auth import logout
 from django.core.urlresolvers import reverse_lazy
-from django.views.generic import RedirectView
+from django.views.generic import RedirectView, ListView, DetailView
 
+from models import UserProfile
 
 def login(request):
     return HttpResponsePermanentRedirect('/')
@@ -14,3 +15,20 @@ class LogoutView(RedirectView):
     def get(self, request, *args, **kwargs):
         logout(request)
         return super(LogoutView, self).get(request, *args, **kwargs)
+
+
+class UsersView(ListView):
+    """
+    Users list
+    """
+    def get_queryset(self):
+        """
+        Users list
+        :return:
+        :rtype: QuerySet
+        """
+        return UserProfile.objects.filter(is_active=True, is_deleted=False)[:30]
+
+
+class UserDetailView(DetailView):
+    model = UserProfile
