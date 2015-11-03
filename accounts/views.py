38d8b1cd8f3c87554a20dade9b885f25b04ecd86
Wojciech Zając <wojciech.zajac@tirasu.com>
2015-11-03 19:17:25 +0100

    users list embeded in home

--- accounts/views.py
+++ accounts/views.py
@@ -5,6 +5,7 @@ from django.views.generic import RedirectView, ListView, DetailView
 
 from models import UserProfile
 
+
 def login(request):
     return HttpResponsePermanentRedirect('/')
 
@@ -15,19 +16,6 @@ class LogoutView(RedirectView):
     def get(self, request, *args, **kwargs):
         logout(request)
         return super(LogoutView, self).get(request, *args, **kwargs)
-
-
-class UsersView(ListView):
-    """
-    Users list
-    """
-    def get_queryset(self):
-        """
-        Users list
-        :return:
-        :rtype: QuerySet
-        """
-        return UserProfile.objects.filter(is_active=True, is_deleted=False)[:30]
 
 
 class UserDetailView(DetailView):
