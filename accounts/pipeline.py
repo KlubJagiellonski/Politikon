f08c8844b4d20c7dee4eab99ba98bcf28e4c3dc8
Piotr Peczek <ppeczek@gmail.com>
2016-03-01 00:41:14 +0000

    error handling in sending emails

--- accounts/pipeline.py
+++ accounts/pipeline.py
@@ -71,7 +71,11 @@ def save_profile(strategy, user, response, details, is_new=False,
                         subject = u'Politikon - nowy użytkownik'
                         message = u'Użytkownik czeka na akceptację. Aktywuj go pod adresem: https://www.politikon.org.pl/admin/accounts/userprofile/'
                         from_email = settings.DEFAULT_EMAIL_FROM
-                        send_mail(subject, message, from_email, recipent_list)
+			try:
+                            send_mail(subject, message, from_email, recipent_list)
+                        except:
+			    # TODO: handle error
+			    print("Error: couldn't send emals")
             else:
                 user.is_active = True
 
@@ -116,7 +120,11 @@ def save_profile(strategy, user, response, details, is_new=False,
                         subject = u'Politikon - nowy użytkownik'
                         message = u'Użytkownik czeka na akceptację. Aktywuj go pod adresem: https://www.politikon.org.pl/admin/accounts/userprofile/'
                         from_email = settings.DEFAULT_EMAIL_FROM
-                        send_mail(subject, message, from_email, recipent_list)
+			try:
+                            send_mail(subject, message, from_email, recipent_list)
+                        except:
+			    # TODO: handle error
+			    print("Error: couldn't send emals")
             else:
                 user.is_active = True
 
