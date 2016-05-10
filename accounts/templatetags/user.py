f54be7c8e772761c96cb1d922f91d65a2086c908
Piotr Pęczek <ppeczek@gmail.com>
2016-05-10 22:57:28 +0200

    better display of rankings on home site

--- accounts/templatetags/user.py
+++ accounts/templatetags/user.py
@@ -3,9 +3,11 @@ register = template.Library()
 
 
 @register.inclusion_tag('user_home.html')
-def user_home(user):
+def user_home(user, reputation_change, is_formatted=False):
     return {
-        'user': user
+        'user': user,
+        'reputation_change': reputation_change,
+        'is_formatted': is_formatted
     }
 
 
