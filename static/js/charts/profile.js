c6a92aeb074b2e4ee5a3a411e2ef0e7b7ccc4a7c
Piotr Pęczek <ppeczek@gmail.com>
2016-05-15 16:10:32 +0200

    userchart

--- static/js/charts/profile.js
+++ static/js/charts/profile.js
@@ -1,21 +1,32 @@
 //wykres na profil uzytkownika
-    var randomScalingFactor = function(){ return Math.round(Math.random()*100)};
-    var profileChartData = {
-        labels : ["22 września","23 września","24 września","25 września","26 września","27 września","28 września"],
-        datasets : [
-            {
-                    fillColor : "#ea6a2a",
-                    strokeColor : "#fff",
-                    pointColor : "#fff",
-                    pointStrokeColor : "#ea6a2a",
-                    pointHighlightFill : "#fff",
-                    pointHighlightStroke : "rgba(220,220,220,1)",
-                    data : [randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor()]
-            }
-        ]
-    }
+function profileChart(data) {
+    if (data) {
+        var profileChartData = {
+            labels: data.labels,
+            datasets: [
+                {
+                    fillColor: "#ea6a2a",
+                    strokeColor: "#fff",
+                    pointColor: "#fff",
+                    pointStrokeColor: "#ea6a2a",
+                    pointHighlightFill: "#fff",
+                    pointHighlightStroke: "rgba(220,220,220,1)",
+                    data: data.points
+                }
+            ]
+        };
 
-    window.onload = function betfeed(){
-        var ctx = document.getElementById("profile-canvas").getContext("2d");
-        window.myLine = new Chart(ctx).Line(profileChartData, { responsive: true,showScale: false, scaleShowLabels: false, datasetStrokeWidth : 6, bezierCurveTension : 0.5, pointDotRadius : 6, pointHitDetectionRadius : 40});
+        window.onload = function betfeed() {
+            var ctx = document.getElementById("profile-canvas").getContext("2d");
+            window.myLine = new Chart(ctx).Line(profileChartData, {
+                responsive: true,
+                showScale: false,
+                scaleShowLabels: false,
+                datasetStrokeWidth: 6,
+                bezierCurveTension: 0.5,
+                pointDotRadius: 6,
+                pointHitDetectionRadius: 40
+            });
+        }
+    }
 }
