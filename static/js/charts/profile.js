2f4b8dd4e62f20013b69c69f7af8a4f4e1f217e9
Piotr Pęczek <ppeczek@gmail.com>
2016-05-15 16:58:52 +0200

    Ranking chart

--- static/js/charts/profile.js
+++ static/js/charts/profile.js
@@ -1,32 +1,30 @@
 //wykres na profil uzytkownika
 function profileChart(data) {
-    if (data) {
-        var profileChartData = {
-            labels: data.labels,
-            datasets: [
-                {
-                    fillColor: "#ea6a2a",
-                    strokeColor: "#fff",
-                    pointColor: "#fff",
-                    pointStrokeColor: "#ea6a2a",
-                    pointHighlightFill: "#fff",
-                    pointHighlightStroke: "rgba(220,220,220,1)",
-                    data: data.points
-                }
-            ]
-        };
+    var profileChartData = {
+        labels: data.labels,
+        datasets: [
+            {
+                fillColor: "#ea6a2a",
+                strokeColor: "#fff",
+                pointColor: "#fff",
+                pointStrokeColor: "#ea6a2a",
+                pointHighlightFill: "#fff",
+                pointHighlightStroke: "rgba(220,220,220,1)",
+                data: data.points
+            }
+        ]
+    };
 
-        window.onload = function betfeed() {
-            var ctx = document.getElementById("profile-canvas").getContext("2d");
-            window.myLine = new Chart(ctx).Line(profileChartData, {
-                responsive: true,
-                showScale: false,
-                scaleShowLabels: false,
-                datasetStrokeWidth: 6,
-                bezierCurveTension: 0.5,
-                pointDotRadius: 6,
-                pointHitDetectionRadius: 40
-            });
-        }
+    window.onload = function betfeed() {
+        var ctx = document.getElementById("profile-canvas").getContext("2d");
+        window.myLine = new Chart(ctx).Line(profileChartData, {
+            responsive: true,
+            showScale: false,
+            scaleShowLabels: false,
+            datasetStrokeWidth: 6,
+            bezierCurveTension: 0.5,
+            pointDotRadius: 6,
+            pointHitDetectionRadius: 40
+        });
     }
 }
