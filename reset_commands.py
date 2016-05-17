d7d47ea804915603674d1d853f83af239dc0ed78
Wojciech Zając <wojciech.zajac@tirasu.com>
2016-05-17 19:18:13 +0200

    script

--- reset_commands.py
+++ reset_commands.py
@@ -5,13 +5,19 @@ from constance import config
 from django.utils.timezone import now
 
 
-for t in Transaction.objects.filter(event__is_in_progress=True):
+for t in Transaction.objects.filter(type=Transaction.TRANSACTION_TYPE_CHOICES.TOPPED_UP_BY_APP):
     t.delete()
-for b in Bet.objects.filter(event__is_in_progress=True):
+
+
+for b in Bet.objects.filter(event__outcome=Event.EVENT_OUTCOME_CHOICES.IN_PROGRESS):
     b.delete()
+
+
 for e in Event.objects.ongoing_only_queryset():
     e.recalculate_prices()
+
+
 for u in UserProfile.objects.get_users():
-    u.active_date = now() - timedelta(years=1)
+    u.active_date = now() - timedelta(days=366)
     u.save()
     u.topup_cash(config.STARTING_CASH)
