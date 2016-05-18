9a7ecfb4600ae7218b438827730a238443bcdb98
Piotr Pęczek <ppeczek@gmail.com>
2016-05-18 14:55:20 +0200

    Hotfix for fb and tt registration

--- accounts/pipeline.py
+++ accounts/pipeline.py
@@ -18,8 +18,7 @@ logger = logging.getLogger(__name__)
 
 
 @partial
-def save_profile(strategy, user, response, details, is_new=False,
-                 *args, **kwargs):
+def save_profile(strategy, user, response, details, is_new=False, *args, **kwargs):
     """
     Saves profile when logging with social auth.
     When new account then gets profile picture and checks number of
@@ -37,50 +36,51 @@ def save_profile(strategy, user, response, details, is_new=False,
     :param is_new: is it new account
     """
     #  print(strategy.storage)
-    #  print(user)
-    #  print(response)
-    #  print(details)
-    #  print(is_new)
-    #  print(strategy)
+    print(user)
+    print(response)
+    print(details)
+    print(is_new)
+    print(strategy)
     #  uid = kwargs['uid']
 
     backend = kwargs['backend']
 
     if backend.name == 'twitter':
         if is_new or not user.is_active:
-            playing_followers_count = 0
-            tuser = User.remote.fetch(response.get('screen_name', ''))  # TODO: replace default value
-            followers = tuser.fetch_followers(all=True)
-            for follower in followers:
-                try:
-                    follower_profile = UserProfile.objects.get('twitter_user_id', follower.id)
-                except:
-                    pass
-                else:
-                    if follower_profile.is_active:
-                        playing_followers_count += 1
-
-            if playing_followers_count < config.REQUIRED_FRIENDS_THRESHOLD:
-                user.is_active = False
-                messages.warning(strategy.request,
-                                 _('Your account is inactive. Wait for administrator approval.'))
-                if is_new:
-                    recipent_list = []
-                    for admin in UserProfile.objects.get_admins():
-                        if admin.email:
-                            recipent_list.append(admin.email)
-                    if len(recipent_list) > 0:
-                        subject = u'Politikon - nowy użytkownik'
-                        message = u'Użytkownik czeka na akceptację. Aktywuj go pod ' + \
-                            'adresem: https://www.politikon.org.pl/admin/accounts/userprofile/'
-                        try:
-                            from_email = settings.DEFAULT_EMAIL_FROM
-                            send_mail(subject, message, from_email, recipent_list)
-                        except:
-                            # TODO: handle error
-                            logger.exception("Error: couldn't send emails")
-            else:
-                user.is_active = True
+            # playing_followers_count = 0
+            # tuser = User.remote.fetch(response.get('screen_name', ''))  # TODO: replace default value
+            # followers = tuser.fetch_followers(all=True)
+            # print(followers)
+            # for follower in followers:
+            #     try:
+            #         follower_profile = UserProfile.objects.get('twitter_user_id', follower.id)
+            #     except:
+            #         pass
+            #     else:
+            #         if follower_profile.is_active:
+            #             playing_followers_count += 1
+            #
+            # if playing_followers_count < config.REQUIRED_FRIENDS_THRESHOLD:
+            #     user.is_active = False
+            #     messages.warning(strategy.request,
+            #                      _('Your account is inactive. Wait for administrator approval.'))
+            #     if is_new:
+            #         recipent_list = []
+            #         for admin in UserProfile.objects.get_admins():
+            #             if admin.email:
+            #                 recipent_list.append(admin.email)
+            #         if len(recipent_list) > 0:
+            #             subject = u'Politikon - nowy użytkownik'
+            #             message = u'Użytkownik czeka na akceptację. Aktywuj go pod ' + \
+            #                 u'adresem: https://www.politikon.org.pl/admin/accounts/userprofile/'
+            #             try:
+            #                 from_email = settings.DEFAULT_EMAIL_FROM
+            #                 send_mail(subject, message, from_email, recipent_list)
+            #             except:
+            #                 # TODO: handle error
+            #                 logger.exception("Error: couldn't send emails")
+            # else:
+            user.is_active = True
 
             user.name = response.get('name', '')    # TODO: replace default value
             user.twitter_user_id = response['id']
@@ -100,38 +100,39 @@ def save_profile(strategy, user, response, details, is_new=False,
 
     if backend.name == 'facebook':
         if is_new or not user.is_active:
-            playing_friends_count = 0
-            for friend in response.get('friends', {}).get('data', {}):
-                try:
-                    friend_profile = UserProfile.objects.get('facebook_user_id', friend['id'])
-                except:
-                    pass
-                else:
-                    if friend_profile.is_active:
-                        playing_friends_count += 1
-
-            if playing_friends_count < config.REQUIRED_FRIENDS_THRESHOLD:
-                user.is_active = False
-                messages.warning(strategy.request,
-                                 _('Your account is inactive. Wait for administrator approval.'))
-                if is_new:
-                    recipent_list = []
-                    for admin in UserProfile.objects.get_admins():
-                        if admin.email:
-                            recipent_list.append(admin.email)
-                    if len(recipent_list) > 0:
-                        subject = u'Politikon - nowy użytkownik'
-                        message = u'Użytkownik czeka na akceptację. Aktywuj go pod ' + \
-                            'adresem: https://www.politikon.org.pl/admin/accounts/userprofile/'
-                        try:
-                            from_email = settings.DEFAULT_EMAIL_FROM
-                            # TODO: fix it: https://trello.com/c/58h6fUgM/254-b-ad-wysy-ania-maili-na-heroku
-                            # send_mail(subject, message, from_email, recipent_list)
-                        except:
-                            # TODO: handle error
-                            logger.exception("Error: couldn't send emails")
-            else:
-                user.is_active = True
+            # playing_friends_count = 0
+            # print(response.get('friends', {}))
+            # for friend in response.get('friends', {}).get('data', {}):
+            #     try:
+            #         friend_profile = UserProfile.objects.get('facebook_user_id', friend['id'])
+            #     except:
+            #         pass
+            #     else:
+            #         if friend_profile.is_active:
+            #             playing_friends_count += 1
+            #
+            # if playing_friends_count < config.REQUIRED_FRIENDS_THRESHOLD:
+            #     user.is_active = False
+            #     messages.warning(strategy.request,
+            #                      _('Your account is inactive. Wait for administrator approval.'))
+            #     if is_new:
+            #         recipent_list = []
+            #         for admin in UserProfile.objects.get_admins():
+            #             if admin.email:
+            #                 recipent_list.append(admin.email)
+            #         if len(recipent_list) > 0:
+            #             subject = u'Politikon - nowy użytkownik'
+            #             message = u'Użytkownik czeka na akceptację. Aktywuj go pod ' + \
+            #                 u'adresem: https://www.politikon.org.pl/admin/accounts/userprofile/'
+            #             try:
+            #                 from_email = settings.DEFAULT_EMAIL_FROM
+            #                 # TODO: fix it: https://trello.com/c/58h6fUgM/254-b-ad-wysy-ania-maili-na-heroku
+            #                 send_mail(subject, message, from_email, recipent_list)
+            #             except:
+            #                 # TODO: handle error
+            #                 logger.exception("Error: couldn't send emails")
+            # else:
+            user.is_active = True
 
             user.name = details['fullname']
             user.facebook_user = user.facebook_user_id = response['id']
