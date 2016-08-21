79ae5fe417379bf73822e2938c0ad126e36d745a
Julian Volodia <julianvolodia@gmail.com>
2016-08-21 12:09:31 +0200

    fix docker_run.sh and add docker_bye.sh
    this closes KlubJagiellonski/Politikon#246

---
+++ docker_bye.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+NAME='politikon'
+DB_NAME='politikon_db'
+PSQL_VER='postgres:9.4.1'
+NODE_NAME='politikon_instance'
+DOCKER_RUN='./docker_run.sh'
+
+function pri {
+    echo " [${1}] $2"
+}
+
+echo " Please be patient..."
+
+if   ! docker ps -a | grep $DB_NAME >/dev/null; then
+    pri 'db' "$DB_NAME do not exist - did you run $DOCKER_RUN ?"
+elif ! docker ps -a | grep $DB_NAME | grep Up >/dev/null; then
+    pri 'db' "nothing to do - $DB_NAME not running"
+else
+    pri 'db' "stopping $DB_NAME..."
+    docker stop $DB_NAME
+fi
+
+if   ! docker ps -a | grep $NODE_NAME >/dev/null; then
+    pri 'vm' "$NODE_NAME do not exist - did you run $DOCKER_RUN ?"
+elif ! docker ps -a | grep $NODE_NAME | grep Up >/dev/null; then
+    pri 'vm' "nothing to do - $NODE_NAME not running"
+else
+    pri 'vm' "stopping $NODE_NAME..."
+    docker stop $NODE_NAME
+fi
+
+echo " bye, bye, bye..."
+
--- docker_run.sh
+++ docker_run.sh
@@ -1,18 +1,41 @@
 #!/usr/bin/env bash
 
-if ! docker ps -a | grep politikon_db >/dev/null; then
-    docker run -d --name politikon_db postgres:latest
-    echo "New database created" ;
+NAME='politikon'
+DB_NAME='politikon_db'
+PSQL_VER='postgres:latest'
+NODE_NAME='politikon_instance'
+
+function pri {
+    echo " [${1}] $2"
+}
+
+if   ! docker ps -a | grep $DB_NAME >/dev/null; then
+    pri 'db' "$DB_NAME creating and running"
+    docker run -d --name $DB_NAME $PSQL_VER
+elif ! docker ps -a | grep $DB_NAME | grep Up >/dev/null; then
+    pri 'db' "existing $DB_NAME starting..."
+    docker start $DB_NAME
 else
-    docker start politikon_db ;
-    echo "Started existing database" ;
+    pri 'db' "nothing to do - $DB_NAME is running..."
 fi
 
-if ! docker ps -a | grep politikon_instance >/dev/null; then
-    docker run -d --dns 8.8.8.8 --dns 8.8.4.4 -it -v `pwd`:/app -p 2233:22 -p 8000:8000 --name politikon_instance --link politikon_db:postgres politikon /bin/bash;
-    echo "politikon_instance created" ;
-else
-    docker start politikon_instance ;
-    echo "Started existing politikon instance" ;
+if ! docker ps -a | grep $DB_NAME | grep Up >/dev/null; then
+    pri '!!' "cannot start $DB_NAME - exiting"
+    exit 1
 fi
-docker attach politikon_instance
+
+if   ! docker ps -a | grep $NODE_NAME >/dev/null; then
+    pri 'vm' "$NODE_NAME creating and starting..."
+    docker run -d --dns 8.8.8.8 --dns 8.8.4.4 -t -v `pwd`:/app -p 2233:22 -p 8000:8000 --name $NODE_NAME --link ${DB_NAME}:postgres $NAME /bin/bash
+elif ! docker ps -a | grep $NODE_NAME | grep Up >/dev/null; then
+    pri 'vm' "existing $NODE_NAME starting..."
+    docker start $NODE_NAME
+fi
+
+pri 'vm' "getting into $NODE_NAME"
+docker exec -it $NODE_NAME bash
+
+true
+
+echo " tip: after work you could use ./docker_bye.sh"
+
