f29e2d1949f307190b0e6df69082e8f6265b6c1c
Wojciech Zając <wojciech.zajac@tirasu.com>
2017-05-21 20:14:05 +0200

    mechanism for usage translated js files

---
+++ static/js/locale/en.js
@@ -0,0 +1,14 @@
+/**
+ * Created by orkan on 21.05.17.
+ * Translation on English language
+ * Format: {'keyName': 'English text'}
+ */
+var words = {
+    'yes': 'Yes',
+    'no': 'No',
+    'youYes': 'You have YES bets',
+    'youNo': 'You have NO bets',
+    'buyYes': 'Buy more YES',
+    'buyNo': 'Buy more NO',
+    'sellBet': 'sell bet'
+};
